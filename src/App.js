783051708121d5815d5f9381645929e84fcdd1bd
Yoshi <yoshimejia00@gmail.com>
2021-12-17 08:02:43 -0500

    displaying navbar to site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { MainContainer } from './containers/MainContainer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MainContainer />
+        <Navbar />
 
       </header>
     </div>
