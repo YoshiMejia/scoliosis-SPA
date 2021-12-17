1dd424813482d321a5c57cab1a536fa425a69102
Yoshi <yoshimejia00@gmail.com>
2021-12-17 10:37:32 -0500

    added more info to About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 export function About() {
     return (
       <div style={{ padding: 20 }}>
-        <h2>Scoliosis Help Page</h2>
-        <p>Welcome!</p>
-        <p>This site is for anyone looking for help on managing their scoliosis.</p>
-        <p>Click on one of the links (above/below) to see our info!</p>
+        <h1>Scoliosis Help Page</h1>
+        <h3>Welcome!</h3>
+        <p>This site is for anyone looking for help on managing their scoliosis.
+        < br/>
+        <text style={{fontStyle: 'italic'}}> According to MayoClinic: "Scoliosis is a sideways curvature of the spine that most often is diagnosed in adolescents. [...] The cause of most childhood scoliosis is unknown.
+        < br/>
+         Most cases of scoliosis are mild, but some curves worsen as children grow. Severe scoliosis can be disabling."</text>
+         < br/>
+        Click on one of the links above to see our info!</p>
       </div>
     );
   }
