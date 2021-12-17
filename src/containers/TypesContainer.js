f91a570c3da787aefaaeff89b799e51562c391af
Yoshi <yoshimejia00@gmail.com>
2021-12-17 10:38:07 -0500

    building TypesContainer to hold more data

---
+++ src/containers/TypesContainer.js
@@ -0,0 +1,18 @@
+
+export const TypesContainer = () => {
+
+    return(
+        <div id="types-container">
+            <h1>Types of Scoliosis</h1>
+            <p>
+                
+            </p>
+                  {/* 
+                  container will hold Types.js which will be components of data holding info on S curve, C curve, etc. 
+                  onClick of each link, an info box will open up and hold more info on each curve. there will be links to the excercises for each Type.
+                  
+                  */}
+              </div>
+    )
+
+}
