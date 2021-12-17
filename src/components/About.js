1a2231dcc00f0cd037572fb1be376662a22b8fec
Yoshi <yoshimejia00@gmail.com>
2021-12-17 08:02:52 -0500

    created about/main page bio

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+export function About() {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>Scoliosis Help Page</h2>
+        <p>Welcome!</p>
+        <p>This site is for anyone looking for help on managing their scoliosis.</p>
+        <p>Click on one of the links (above/below) to see our info!</p>
+      </div>
+    );
+  }
