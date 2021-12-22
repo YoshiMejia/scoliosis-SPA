b82130acdffc5fefca4addbc4e86a23eb46cf7e0
Yoshi <yoshimejia00@gmail.com>
2021-12-22 10:21:53 -0500

    switched to class component so i can use state for conditionally rendering

--- src/containers/TypesContainer.js
+++ src/containers/TypesContainer.js
@@ -1,20 +1,23 @@
+import React from 'react';
 import types_of_scoly from '../types_of_scoly.jpeg';
+import { Thoracic } from '../components/Thoracic';
 
-export const TypesContainer = () => {
+   class TypesContainer extends React.Component {
+    render(){
+        return(
+            <div id="types-container">
+                <h1>Types of Scoliosis</h1>
+                    {<img src={types_of_scoly} alt="types_of_scoliosis" className='type_pic'/>}
+                        <Thoracic />
+                    {/* 
+                    container will hold Types.js which will be components of data holding info on each curve in the photo. 
 
-    return(
-        <div id="types-container">
-            <h1>Types of Scoliosis</h1>
-            {<img src={types_of_scoly} alt="types_of_scoliosis" className='type_pic'/>}
-            <p>
-
-            </p>
-                  {/* 
-                  container will hold Types.js which will be components of data holding info on each curve in the photo. 
-                  onClick of each link, an info box will open up and hold more info on each curve. there will be links to the excercises for each Type.
-                  
-                  */}
-              </div>
+                    onClick of each link, an info box will open up and hold more info on each curve. there will be links to the excercises for each Type.
+                    
+                    */}
+            </div>
     )
+}
 
 }
+export default TypesContainer
