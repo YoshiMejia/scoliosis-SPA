82e4c70623dc0f3961fea0d0da2dd66ba4088ec3
Yoshi <yoshimejia00@gmail.com>
2021-12-22 10:21:07 -0500

    component for site credits

---
+++ src/components/Bibliography.js
@@ -0,0 +1,13 @@
+export function Bibliography(){
+    return(
+        <div className="bibliography-div">
+            <h1>
+                Credits:
+            </h1>
+            <p>The data on this site is from multiple sources. At no point should the information found on this site be used in place of your doctors own advice. Please use this as a guide or for supplemental information, but I reccomend seeing a sports medicine doctor for physical therapy and creating your own plans for your specific ailments.</p>
+            <ul>
+                <li>Thoracic data is from <a href={'https://scoliosisinstitute.com/thoracic-scoliosis/'}>Scoliosis Institute</a> </li>
+            </ul>
+        </div>
+    )
+}
