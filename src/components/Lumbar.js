f06e40d309a91d19b49183dcbbdb84a60a2c4d37
Yoshi <yoshimejia00@gmail.com>
2021-12-23 10:27:42 -0500

    created new component for Lumbar curve

---
+++ src/components/Lumbar.js
@@ -0,0 +1,28 @@
+export function Lumbar(){
+    return(
+        <div className="lumbar-div">
+            <h1>What is Lumbar Scoliosis?</h1>
+                <p>Lumbar spine, illustrationOf the several different kinds of Scoliosis, Lumbar Scoliosis appears as a C shaped curve in the lower section of the spine.  Also, this curve may occur by itself, or it may occur with thoracic scoliosis, which typically appears with an S shape to the spine as the two curves form in different directions.
+                <br />
+                In most cases, lower back scoliosis becomes apparent during early to mid-childhood. However, in cases of adult degenerative scoliosis, it may also occur along with or as the result of other related conditions.
+                <br />
+                Furthermore, lumbar scoliosis occurs most frequently in the lower back because of degeneration.  In addition, It is not uncommon for older adults, people older than 65, to have this condition. We usually see this condition with spinal stenosis, or narrowing of the spinal canal.  Typically, spinal stenosis pinches the spinal nerves, and thus, makes it difficult for them to function properly. Moreover, our doctors find that back pain occurs with scoliosis in the lumbar region and begins gradually with activity. In conclusion, the curvature of the spine can appear relatively minor, so surgery only occurs when conservative methods fail to stop the pain.
+                <br />
+                </p>
+                <h2>Scoliosis Symptoms</h2>
+                <p>To clarify, the symptoms of lumbar scoliosis differ from person to person and depend on a number of factors.  Also, these include age, the severity of the curve, the location of the curve, and more. Some patients experience little to no scoliosis pain even after years with the condition, while others experience pain right away.
+                <br />
+                Some symptoms of lower back scoliosis include:
+                <br />
+                </p>
+                <ul>
+<li>Uneven shoulders</li>
+Unusually raised hips</li>
+Uneven rib cage alignment</li>
+Uneven waist</li>
+Body leaning to one side</li>
+Back pain</li>
+</ul>
+        </div>
+    )
+}
