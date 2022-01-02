4a21b80c4c2a88735faab2a401648c278fe93de2
Yoshi <yoshimejia00@gmail.com>
2022-01-02 08:28:05 -0500

    created types bio for more info on curve types

---
+++ src/components/types_components/Types_Bio.js
@@ -0,0 +1,18 @@
+export function TypesBio(){
+    return(
+        <div className="types_bio">
+            <h1>What Sets the Different Types of Scoliosis Apart?</h1>
+
+            <p>There are many ways to determine the differences between the various forms of scoliosis.  The most common method uses etiology or the underlying cause for the condition. Most experts feel there are three categories of scoliosis.  idiopathic, congenital, and neuromuscular.
+            
+            Idiopathic is the most common type of scoliosis. This means that the cause is unknown or that there is no single factor that contributes to the development of the disease. Get recommendations for treating idiopathic scoliosis directly to your email.
+            
+            Congenital forms of scoliosis result from a spinal defect present at birth. This is usually detected at a much earlier age than idiopathic forms of scoliosis.
+            
+            Neuromuscular scoliosis is spinal curvature that develops because of some kind of other diseases.  Examples include muscular dystrophy or cerebral palsy. This form of scoliosis tends to progress much more quickly than others.
+            
+            Knowing how spinal curvatures are classified is important. It may provide a foundation of knowledge on how to treat a specific type of scoliosis.
+            </p>
+        </div>
+    )
+}
