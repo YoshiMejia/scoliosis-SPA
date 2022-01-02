78e125495c034df87d41ce7d596ebb3b7019d536
Yoshi <yoshimejia00@gmail.com>
2022-01-02 08:11:50 -0500

    building workouts container

---
+++ src/containers/WorkoutsContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class WorkoutsContainer extends React.Component {
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div className='workouts-container'>
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
+
+export default WorkoutsContainer
