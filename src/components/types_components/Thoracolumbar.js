aa2a0590fd1abcd1be273a974521a22238614a97
Yoshi <yoshimejia00@gmail.com>
2022-01-02 08:27:49 -0500

    created nested folder for Types, to keep components organized

---
+++ src/components/types_components/Thoracolumbar.js
@@ -0,0 +1,11 @@
+// check if thoracolumbar is the same as Levoscoliosis 
+export function Thoracolumbar (){
+    return (
+        <div className="thoracolumbar-div">
+            <h1>Thoracolumbar Scoliosis</h1>
+                <p>With thoracolumbar scoliosis, the curve includes vertebrae from both the lower thoracic and upper lumbar parts of the spine. It is often congenital, occurring in utero during the third to sixth week, and detectable at birth. It may even be the secondary effect of a neuromuscular condition (such as spina bifida or cerebral palsy).
+            <br />
+                Thoracolumbar scoliosis usually involves a curve toward the right and occurs more often in females than in males.</p>
+        </div>
+    )
+} 
