e070eb83d7f029c93e0338870fbebd87543a55ac
Yoshi <yoshimejia00@gmail.com>
2022-04-05 13:46:19 -0400

    importing react, fixed spacing

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,18 @@
-export function About() {
-    return (
-      <div style={{ padding: 20 }}>
-        <h1>Scoliosis Help Page</h1>
-        <h3>Welcome!</h3>
-        <p>This site is for anyone looking for help on managing their scoliosis.
-        < br/>
-        <text style={{fontStyle: 'italic'}}> According to MayoClinic: "Scoliosis is a sideways curvature of the spine that most often is diagnosed in adolescents. [...] The cause of most childhood scoliosis is unknown.
-        < br/>
-         Most cases of scoliosis are mild, but some curves worsen as children grow. Severe scoliosis can be disabling."</text>
-         < br/>
-        Click on one of the links above to see our info!</p>
-      </div>
-    );
-  }
+import React from "react";
+
+export const About = () => (
+  <div style={{ padding: 20 }}>
+    <h1>Scoliosis Help Page</h1>
+    <h3>Welcome!</h3>
+    
+    <p>This site is for anyone looking for help on managing their scoliosis.
+    < br/>
+    <text style={{fontStyle: 'italic'}}> According to MayoClinic: "Scoliosis is a sideways curvature of the spine that most often is diagnosed in adolescents. [...] The cause of most childhood scoliosis is unknown.
+    
+    < br/>
+    Most cases of scoliosis are mild, but some curves worsen as children grow. Severe scoliosis can be disabling."</text>
+
+    < br/>
+    Click on one of the links above to see our info!</p>
+  </div>
+)
