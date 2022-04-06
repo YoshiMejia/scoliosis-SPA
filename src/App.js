2b8fad74ada1f48b3144eb68974ae5018cb96106
Yoshi <yoshimejia00@gmail.com>
2022-04-05 22:06:42 -0400

    moved Routes to render here instead of  navbar.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { About } from './components/About';
+import { Bibliography } from './components/Bibliography';
+import TypesContainer from './containers/TypesContainer';
 import Navbar from './components/Navbar';
 
 export default class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <Navbar store={this.props.store} />
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div id='page-body'>
+            <Routes>
+              <Route path="/" element={<About />} exact />
+              <Route path="/bibliography" element={<Bibliography />} />
+              <Route path="/types" element={<TypesContainer />}/>
+            </Routes>
+          </div>
+        </div>
+      </Router>
     );
   }
 
