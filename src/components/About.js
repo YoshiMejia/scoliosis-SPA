2ddeddc2c27c9f7c8c618f6d34494872f8f79432
Yoshi <yoshimejia00@gmail.com>
2022-04-05 22:07:03 -0400

    added page breaks and moved lines

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,13 @@ export const About = () => (
     
     <p>This site is for anyone looking for help on managing their scoliosis.
     < br/>
+    Click on one of the links above to see our info!</p>
+    < br/>
     <text style={{fontStyle: 'italic'}}> According to MayoClinic: "Scoliosis is a sideways curvature of the spine that most often is diagnosed in adolescents. [...] The cause of most childhood scoliosis is unknown.
     
     < br/>
     Most cases of scoliosis are mild, but some curves worsen as children grow. Severe scoliosis can be disabling."</text>
 
     < br/>
-    Click on one of the links above to see our info!</p>
   </div>
 )
