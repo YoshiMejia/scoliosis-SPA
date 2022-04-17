c5c23694a3c2b30140688b5bec3231200fd9506c
Yoshi <yoshimejia00@gmail.com>
2022-04-16 21:18:33 -0400

    removed commented code

--- src/containers/TypesContainer.js
+++ src/containers/TypesContainer.js
@@ -5,57 +5,11 @@ import { TypesBio } from '../components/types_components/Types_Bio';
 const TypesContainer = () => {
 
     return(
-        <>
+        <div className='types-container'>
             {<TypesBio />}
-        </>
+        </div>
     )
 }
 
 export default TypesContainer
-    
-    // return(
-    //     <div id="types-container">
-    //         <br />
-    //             {<img src={workout_spine} alt="workouts_pic" className='workouts_pic'/>}
-    //         < TypesBio/>
-    //             {/* {<img src={types_of_scoly} alt="types_of_scoliosis" className='type_pic'/>} */}
-    
-    //             <p className='thoracic-button' onClick={hideForm()}>Thoracic Curve</p>
-    //                 {this.state.showThoracic && <Thoracic />}
-    //             <p className='lumbar-button' onClick={hideForm()}>Lumbar Curve</p>
-    //                 {this.state.showLumbar && <Lumbar />}
-    //             <p className='thoracolumbar-button' onClick={hideForm()}>Thoracolumbar Curve</p>
-    //                 {this.state.showThoracolumbar && <Thoracolumbar />}
-    //     </div>
-    // )
-// mapState = state => {
-    //     return{
-        
-        //     }
-        // }
-        // export default connect(null, mapDispatch)(TypesContainer)
-        // const hideForm = (e) => {
-        //     switch(e.target.className){
-        //     case 'thoracic-button':
-        //         this.setState({
-        //             ...this.state,
-        //             showThoracic: !this.state.showThoracic
-        //         })
-        //         break;
-        //     case 'lumbar-button':
-        //         this.setState({
-        //             ...this.state,
-        //             showLumbar: !this.state.showLumbar
-        //         })
-        //         break;
-        //     case 'thoracolumbar-button':
-        //         this.setState({
-        //             ...this.state,
-        //             showThoracolumbar: !this.state.showThoracolumbar
-        //         })
-        //         break;
-        //     default: 
-        //         console.log("unknown category");
-        //         break;
-        //         }
-        //     }
+   
