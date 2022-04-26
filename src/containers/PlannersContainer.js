1010b8ad232a5a6b829f3750aca07f2770954be6
Yoshi <yoshimejia00@gmail.com>
2022-04-25 21:58:53 -0400

    displaying test header

---
+++ src/containers/PlannersContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ const PlannersContainer = () => {
+   
+    return(
+        <div className='planners-container'>
+            <h1>Planners container</h1>
+            
+        </div>
+    )
+}
+
+export default PlannersContainer
