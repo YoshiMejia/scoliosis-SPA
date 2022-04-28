caeec3af61ed28b12fc5e55f5795df57696488b3
Yoshi <yoshimejia00@gmail.com>
2022-04-27 21:13:40 -0400

    added useEffect hook for axios fetch

--- src/components/workouts_components/Planners.js
+++ src/components/workouts_components/Planners.js
@@ -1,10 +1,26 @@
-import React, { useState} from "react";
-import axios from 'axios';
+import React, { useEffect, useState} from "react";
+import Axios from 'axios';
 
 export function Planners (){
     const [planners, setPlanners] = useState([])
     const [workouts, setWorkouts] = useState([])
      
+    
+    useEffect(() => {
+        getPlanners()
+    }, [])
+    const url = 'http://127.0.0.1:3000/'
+    const getPlanners = () => {
+        debugger
+        console.log('sfhkj');
+        Axios.get(`http://127.0.0.1:3000/planners`)
+        .then((resp)=>{
+            const allPlanners = resp.data
+            setPlanners(allPlanners)
+        })
+        .catch(error => console.error(`Error: ${error}`))
+    }
+    
     return(
         <div id="planners-container">
             <h1>Planners container</h1>
