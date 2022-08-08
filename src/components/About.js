cbfa55a7812efacc047b9b8f062e9dc73c6e5d4c
Yoshi <yoshimejia00@gmail.com>
2022-08-07 20:37:25 -0400

    styling: added divs for styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,32 @@
 import React from "react";
+import no_more_pain from '../media/no_more_pain.jpeg'
 
 export const About = () => (
-  <div style={{ padding: 20, textAlign: "left" }}>
-    <h1>Scoliosis Advice</h1>
-    <h3>Welcome!</h3>
-    
-    <p>This site is for anyone looking for help on managing their scoliosis.
-    < br/>
-    Click on one of the links above to see our info!</p>
-    < br/>
-    <text style={{fontStyle: 'italic'}}> According to MayoClinic: "Scoliosis is a sideways curvature of the spine that most often is diagnosed in adolescents. [...] The cause of most childhood scoliosis is unknown.
-    
-    < br/>
-    Most cases of scoliosis are mild, but some curves worsen as children grow. Severe scoliosis can be disabling."</text>
+  <div id="main">
+    <div>
+      <h1>Scoliosis Advice</h1>
+    </div>
+
+  <div>
+    <img src={no_more_pain} alt='street sign that says no more pain' id="nomore_sign"/>
+  </div>
+    <div>
+      <h3>Welcome!</h3>
+    </div>
+
+    <div>
+      <p>This site is for anyone looking for help on managing their scoliosis.
+      < br/>
+      Click on one of the links above to see our info!</p>
+      < br/>
+    </div>
+
+    <text>
+      According to MayoClinic: "Scoliosis is a sideways curvature of the spine that most often is diagnosed in adolescents. 
+      [...] The cause of most childhood scoliosis is unknown.
+      < br/>
+      Most cases of scoliosis are mild, but some curves worsen as children grow. Severe scoliosis can be disabling."
+    </text>
 
     < br/>
   </div>
