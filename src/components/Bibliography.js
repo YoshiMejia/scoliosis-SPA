fee8f32db58fd49275b999421924404eb91dd9e7
Yoshi <yoshimejia00@gmail.com>
2022-08-10 22:19:20 -0400

    added divs for styling

--- src/components/Bibliography.js
+++ src/components/Bibliography.js
@@ -1,15 +1,21 @@
 export function Bibliography(){
     return(
         <div className="bibliography-div">
-            <h1 className="banner">
-                Credits:
-            </h1>
-            <p>The data on this site is from multiple sources. At no point should the information found on this site be used in place of your doctors own advice. Please use this as a guide or for supplemental information, but I reccomend seeing a sports medicine doctor for physical therapy and creating your own plans for your specific ailments.</p>
-            <ul>
-                <li>Thoracic data is from <a href={'https://scoliosisinstitute.com/thoracic-scoliosis/'}>Scoliosis Institute</a> </li>
-                <li>Lumbar Data is from <a href={'https://scoliosisinstitute.com/lumbar-scoliosis/'}>Scoliosis Institute</a> </li>
-                <li>Types Bio data is from <a href ={'https://www.treatingscoliosis.com/blog/scoliosis-types-differences/#what_sets_the_different_types_of_scoliosis_apart'}>Treating Scoliiosis website</a></li>
-            </ul>
+            <div>
+                <h1 className="banner">
+                    Credits:
+                </h1>
+            </div>
+            <div>
+                <p>The data on this site is from multiple sources. At no point should the information found on this site be used in place of your doctors own advice. Please use this as a guide or for supplemental information, but I recommend seeing a sports medicine doctor for physical therapy and creating your own plans for your specific ailments.</p>
+                <div className="credits_list">
+                    <ul>
+                        <li>Thoracic data is from <a href={'https://scoliosisinstitute.com/thoracic-scoliosis/'}>Scoliosis Institute</a> </li>
+                        <li>Lumbar Data is from <a href={'https://scoliosisinstitute.com/lumbar-scoliosis/'}>Scoliosis Institute</a> </li>
+                        <li>Types Bio data is from <a href ={'https://www.treatingscoliosis.com/blog/scoliosis-types-differences/#what_sets_the_different_types_of_scoliosis_apart'}>Treating Scoliiosis website</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
