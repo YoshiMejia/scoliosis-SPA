facfa3150a2b685ce4da28d9fddc8dc366b20d76
Yoshi <yoshimejia00@gmail.com>
2022-08-10 22:28:38 -0400

    moved classname around

--- src/components/Bibliography.js
+++ src/components/Bibliography.js
@@ -8,8 +8,8 @@ export function Bibliography(){
             </div>
             <div>
                 <p>The data on this site is from multiple sources. At no point should the information found on this site be used in place of your doctors own advice. Please use this as a guide or for supplemental information, but I recommend seeing a sports medicine doctor for physical therapy and creating your own plans for your specific ailments.</p>
-                <div className="credits_list">
-                    <ul>
+                <div>
+                    <ul className="credits_list">
                         <li>Thoracic data is from <a href={'https://scoliosisinstitute.com/thoracic-scoliosis/'}>Scoliosis Institute</a> </li>
                         <li>Lumbar Data is from <a href={'https://scoliosisinstitute.com/lumbar-scoliosis/'}>Scoliosis Institute</a> </li>
                         <li>Types Bio data is from <a href ={'https://www.treatingscoliosis.com/blog/scoliosis-types-differences/#what_sets_the_different_types_of_scoliosis_apart'}>Treating Scoliiosis website</a></li>
